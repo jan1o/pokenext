07d5a9cac44f9cf7372948ff74629caf2f433f0a
Janio <juniorjanio2016@gmail.com>
2022-02-17 09:55:43 -0300

    Aula 23 https://www.youtube.com/playlist?list=PLnDvRpP8BnezfJcfiClWskFOLODeqI_Ft

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+import styles from '../styles/About.module.css'
+
+export default function About(){
+  return(
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>lorem</p>
+      <Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
+
+    </div>
+  )
+}
